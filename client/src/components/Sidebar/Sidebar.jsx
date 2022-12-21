20edec64cfaf0efdc320bce83eaa1c8101e2c5cd
Nick Guimarais <Nick.guimarais@gmail.com>
2022-12-21 12:57:20 -0800

    make sidebar close after link is clicked

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,17 +1,24 @@
 /* eslint-disable prettier/prettier */
 
 import { Wrapper, LinkWrapper } from "./Sidebar";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { rickSelectors } from "../../selectors/index";
 import { LinkStyled } from "../Navbar/Navbar";
+import { rickAndMortyActions } from "../../reducers";
 
 export const Sidebar = () => {
   const sidebarOpen = useSelector(rickSelectors.getSidebarState);
+  const dispatch = useDispatch();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    dispatch(rickAndMortyActions.setSidebar(false));
+  };
 
   return (
     sidebarOpen && (
       <Wrapper>
-        <LinkWrapper>
+        <LinkWrapper onClick={handleClick}>
           <LinkStyled to="/characters">Characters</LinkStyled>
           <LinkStyled to="/location">Location</LinkStyled>
           <LinkStyled to="/episodes">Episodes</LinkStyled>
