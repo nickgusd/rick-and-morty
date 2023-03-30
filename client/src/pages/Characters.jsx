9b834f298abe635eea57c3889bacdbc6d4af13b4
Nick Guimarais <Nick.guimarais@gmail.com>
2023-03-29 18:35:25 -0700

    add variables for other parameters

--- client/src/pages/Characters.jsx
+++ client/src/pages/Characters.jsx
@@ -2,8 +2,8 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useLocation, createSearchParams } from "react-router-dom";
 import queryString from "query-string";
-import { gql } from '@apollo/client';
-import { useQuery } from "@apollo/react-hooks"
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/react-hooks";
 
 import ButtonComponent from "../components/Button/Button.jsx";
 import { CharacterCard } from "../components/CharacterCard.jsx";
@@ -22,45 +22,12 @@ import { FilterContainer, Flex } from "../components/Filter/Filter.js";
 import RickToilet from "../assets/rick_and_morty_toilet.png";
 import { title } from "../utils/string.js";
 
-const GET_CHARACTERS_QUERY = gql`
-query {
-  characters {
-    info {
-      count
-      pages
-    }
-    results {
-      id,
-      created,
-      name,
-      image,
-      status,
-      species,
-      type,
-      gender,
-      origin {
-        id,
-        name
-      },
-      location {
-        id,
-        name
-      },
-      episode {
-        id,
-        name
-      }
-    }
-  }
-}
-`
-
 export default function Characters() {
   const navigate = useNavigate();
   const location = useLocation();
   // const [data, setData] = useState([]);
   // const [isLoading, setIsLoading] = useState(false);
-  const [response, setResponse] = useState([])
+  const [response, setResponse] = useState([]);
   const [inputData, setInputData] = useState("");
   const [search, setSearch] = useState(false);
   const [urlParams, setUrlParams] = useState(null);
@@ -71,75 +38,78 @@ export default function Characters() {
   const parse = queryString.parse(location.search);
   const params = urlParams || { name: inputData };
 
-// const query = new URLSearchParams(location.search).get('query');
+  const GET_CHARACTERS_QUERY = gql`
+    query GetCharacters(
+      $page: Int
+      $name: String
+      $status: String
+      $gender: String
+    ) {
+      characters(
+        page: $page
+        filter: { name: $name, status: $status, gender: $gender }
+      ) {
+        info {
+          count
+          pages
+        }
+        results {
+          id
+          created
+          name
+          image
+          status
+          species
+          type
+          gender
+          origin {
+            id
+            name
+          }
+          location {
+            id
+            name
+          }
+          episode {
+            id
+            name
+          }
+        }
+      }
+    }
+  `;
 
-// const test = new URLSearchParams(location.search)
-// console.log("query", query)
-// console.log("test", test)
-const { data, loading, error, refetch } = useQuery(GET_CHARACTERS_QUERY, {
-  variables: { parse }
-})
-
-const characters = data
-
-console.log("characters", characters)
-console.log("loading", loading);
-console.log("error", error)
-// console.log("parse", parse)
-// console.log("data", data)
-
+  const { data, loading, error, refetch } = useQuery(GET_CHARACTERS_QUERY, {
+    variables: { parse },
+  });
 
   useEffect(() => {
     setUrlParams(parse);
     setGender(null);
     setStatus(null);
 
+    if (parse.page) {
+      parse.page = Number(parse.page);
+    }
+    refetch(parse); // Trigger the query with the new parameter
 
-
-    // const { data, loading, error } = useQuery(GET_CHARACTERS_QUERY)
-    // const characters = data
-    // setResponse(data);
-    // const newQuery = new URLSearchParams(location.search).get('query');
-
-    // console.log("newQuery", newQuery)
-
-    console.log("parse", parse)
-      refetch({ query: parse }); // Trigger the query with the new parameter
-  
-    // console.log("characters", characters)
-    // console.log("loading", loading);
-    // console.log("error", error)
-    console.log("response", response)
-    console.log("status", status)
-    console.log("gender", gender)
-    console.log("params", params)
-    console.log("setINputData", setInputData)
-    console.log("setINputData", setSearch)
-
-
-
-      // fetch(`https://rickandmortyapi.com/api/character${location.search}`)
-      //   .then((res) => res.json())
-      //   .then((response) => {
-      //     setIsLoading(true);
-      //     setData(response);
-      //     setUrlParams(null);
-      //   })
-      //   .catch((error) => setError(error))
-        // .finally(() => setTimeout(() => setIsLoading(false), 200));
-    
-
-    
+    //This is the old Rest API fetch
+    // fetch(`https://rickandmortyapi.com/api/character${location.search}`)
+    //   .then((res) => res.json())
+    //   .then((response) => {
+    //     setIsLoading(true);
+    //     setData(response);
+    //     setUrlParams(null);
+    //   })
+    //   .catch((error) => setError(error))
+    // .finally(() => setTimeout(() => setIsLoading(false), 200));
   }, [search, location.search]);
-
-
-  // console.log("data", data)
 
   const onSearch = () => {
     setSearch(!search);
     navigate({
       pathname: "/characters",
-      search: `?${createSearchParams(params)}`,
+      search: `?${createSearchParams({ page: 1, ...params, name: inputData })}`,
     });
     setPage(1);
   };
@@ -206,35 +176,32 @@ console.log("error", error)
     { key: 3, text: "Unknown", value: "unknown" },
   ];
 
-  // console.log("data.error", data.error)
-  console.log("data", data)
-
-  
-  return data && (
-    <Layout characters>
-      <FilterContainer onKeyDown={keyDownHandler}>
-        <SearchWrapper>
-          <Search isLoading={false} onChange={handleChange} />
-          <ButtonComponent onClick={handleClick} primary={true} search />
-        </SearchWrapper>
-        <Flex isMobile>
-          <Filter
-            onChange={handleGenderFilter}
-            currentValue={title(parse.gender)}
-            options={genderOptions}
-            type="Gender"
-            value={gender}
-          />
-          <Filter
-            onChange={handleStatusChange}
-            currentValue={title(parse.status)}
-            options={statusOptions}
-            type="Status"
-            value={status}
-          />
-        </Flex>
-      </FilterContainer>
-      {/* {!error ? (
+  return (
+    data && (
+      <Layout characters>
+        <FilterContainer onKeyDown={keyDownHandler}>
+          <SearchWrapper>
+            <Search isLoading={false} onChange={handleChange} />
+            <ButtonComponent onClick={handleClick} primary={true} search />
+          </SearchWrapper>
+          <Flex isMobile>
+            <Filter
+              onChange={handleGenderFilter}
+              currentValue={title(parse.gender)}
+              options={genderOptions}
+              type="Gender"
+              value={gender}
+            />
+            <Filter
+              onChange={handleStatusChange}
+              currentValue={title(parse.status)}
+              options={statusOptions}
+              type="Status"
+              value={status}
+            />
+          </Flex>
+        </FilterContainer>
+        {/* {!error ? (
         <Header>
           <h1>No Results Found</h1>
         </Header>
@@ -243,27 +210,28 @@ console.log("error", error)
           <h1>Characters</h1>
         </Header>
       )} */}
-      {loading ? (
-        <Loader />
-      ) : (
-        <div>
-          {!error && data && (
-            <>
-              <CharacterCard character={data.characters.results} />
-              <PaginationComponent
-                totalPages={data?.characters?.info?.pages || 0}
-                onChange={onPageChange}
-                activePage={page}
-              />
-            </>
-          )}
-          {/* {!error && (
+        {loading ? (
+          <Loader />
+        ) : (
+          <div>
+            {!error && data && (
+              <>
+                <CharacterCard character={data.characters.results} />
+                <PaginationComponent
+                  totalPages={data?.characters?.info?.pages || 0}
+                  onChange={onPageChange}
+                  activePage={page}
+                />
+              </>
+            )}
+            {/* {!error && (
             <ImageWrapper>
               <img src={RickToilet} alt="rick" />
             </ImageWrapper>
           )} */}
-        </div>
-      )}
-    </Layout>
+          </div>
+        )}
+      </Layout>
+    )
   );
 }
