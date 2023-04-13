d03f126be59a30f635009248bce7e32b9407e2d9
Nick Guimarais <Nick.guimarais@gmail.com>
2023-04-12 18:18:36 -0700

    redploy app

--- client/src/components/Loader/Loader.jsx
+++ client/src/components/Loader/Loader.jsx
@@ -1,7 +1,3 @@
-// import * as React from 'react';
-// import CircularProgress from '@mui/material/CircularProgress';
-// import Box from '@mui/material/Box';
-
 import { Dimmer, Loader, Image } from "semantic-ui-react";
 
 const LoaderComponent = () => (
